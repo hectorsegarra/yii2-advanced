b1c7b616d5f7700c6358439317954dd1adb4245b
hectorsegarra <hector.segarra@outlook.com>
2022-12-25 12:10:30 +0100

    Añadido el idioma español

---
+++ modules/main/messages/es/module.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'Home' => 'Casa',
+    'About' => 'Acerca de',
+    'Contact' => 'Contacto',
+    'Dashboard' => 'Tablero',
+    'Submit' => 'Enviar',
+    'Collapse' => 'Colapso',
+    'Remove' => 'Eliminar',
+    'Name' => 'Nombre',
+    'E-mail' => 'Correo electrónico',
+    'Subject' => 'Asunto',
+    'Body' => 'Cuerpo',
+    'Verification Code' => 'Código de verificación',
+    'Code' => 'Código',
+    'Click on the picture to change the code.' => 'Haga clic en la imagen para cambiar el código.',
+    'If you have business inquiries or other questions, please fill out the following form to contact us. Thank you.' =>
+    'Si tiene consultas de negocios u otras preguntas, complete el siguiente formulario para contactarnos. Gracias.',
+    'Thank you for contacting us. We will respond to you as soon as possible.' =>
+    'Gracias por contactarnos. Nos pondremos en contacto con usted lo antes posible.',
+    'There was an error sending email.' => 'Se produjo un error al enviar el correo electrónico.',
+    'You are not allowed access!' => '¡No se te permite el acceso!',
+    'Welcome, {:username}!' => '¡Bienvenido, {:username}!',
+    'Will close in {n, plural, one{# second} few{# seconds} many{# second} other{# seconds}}.' =>
+    'Se cerrará en {n, plural, one{# segundo} few{# segundos} many{# segundo} other{# segundos}}.'
+];
---
+++ modules/rbac/messages/es/module.php
@@ -0,0 +1,111 @@
+<?php
+
+return [
+    'RBAC' => 'RBAC',
+    'Role Based Access Control' => 'Control de acceso basado en roles',
+    'Auth Item' => 'Auth Item',
+    'Roles' => 'Roles',
+    'Create Role' => 'Crear rol',
+    'Permissions' => 'Permisos',
+    'Create Permission' => 'Crear permiso',
+    'Rules' => 'Reglas',
+    'Create Rule' => 'Crear regla',
+    'Reset rbac' => 'Reiniciar rbac',
+    'Update Role' => 'Actualizar rol',
+    'Update Permission' => 'Actualizar permiso',
+    'View Role' => 'Ver rol',
+    'View Permission' => 'Ver permiso',
+    'Name' => 'Nombre',
+    'Type' => 'Tipo',
+    'Description' => 'Descripción',
+    'Rule Name' => 'Nombre de regla',
+    'Data' => 'Datos',
+    'Created' => 'Creado',
+    'Updated' => 'Actualizado',
+    'Super Administrator' => 'Super administrador',
+    'Administrator' => 'Administrador',
+    'FileStateForm' => 'Formulario de estado de archivo',
+    'Editor' => 'Editor',
+    'User' => 'Usuario',
+    'Access to RBAC management' => 'Acceso a la gestión de RBAC',
+    'FileStateForm Users' => 'Usuarios de FileStateForm',
+    'Access to the admin area' => 'Acceso al área de administración',
+    'Access to Users management' => 'Acceso a la gestión de usuarios',
+    'Access to Posts management' => 'Acceso a la gestión de publicaciones',
+    'Editing your own post' => 'Edición de tu propia publicación',
+    'Permissions by role' => 'Permisos por rol',
+    'Items permissions' => 'Permisos de elementos',
+    'Roles by role' => 'Roles por rol',
+    'Items roles' => 'Roles de elementos',
+    'Role' => 'Rol',
+    'Permission' => 'Permiso',
+    'Rule' => 'Regla',
+    'Assign rights' => 'Asignar derechos',
+    'Assign' => 'Asignar'
+
+    'View' => 'Ver',
+    'Update' => 'Actualizar',
+    'Create' => 'Crear',
+    'Revoke' => 'Revocar',
+    'Save' => 'Guardar',
+    'Delete' => 'Borrar',
+    
+    'Example: moderator' => 'Ejemplo: moderador',
+    'Example: updatePost' => 'Ejemplo: actualizarPublicación',
+    
+    'This name is already taken.' => 'Este nombre ya está tomado.',
+    'Enter name permission.' => 'Introduzca el nombre del permiso.',
+    'Enter name role.' => 'Introduzca el nombre del rol.',
+    
+    'It is allowed to use the Latin alphabet, numbers, dashes and underscores.(A-z,0-1,-,)' =>
+    'Se permite usar el alfabeto latino, números, guiones y subrayados.(A-z,0-1,-,)',
+    
+    'Not a valid request to the method!' => '¡Solicitud no válida al método!',
+    'The data being processed is not an array!' => '¡Los datos que se están procesando no son una matriz!',
+    'There is no data in the POST method!' => '¡No hay datos en el método POST!',
+    
+    'Attention! All previously created permissions and roles will be deleted. Do you really want to perform this action?' => // phpcs:ignore
+    '¡Atención! Todos los permisos y roles creados anteriormente serán eliminados. ¿Realmente deseas realizar esta acción?', // phpcs:ignore
+    'The operation was successful!' => '¡La operación fue exitosa!',
+    'Do you really want to untie all roles?' => '¿Realmente quieres desvincular todos los roles?',
+
+   
+    'You must select in the field «{attribute}».' => 'Debe seleccionar en el campo «{attribute}».',
+    'You must specify the User class in the module settings.' =>
+    'Debe especificar la clase de usuario en la configuración del módulo.',
+    'Are you sure you want to delete the entry?' => '¿Estás seguro de que quieres eliminar la entrada?',
+    'Do you really want to untie the user from the role?' => '¿Realmente quieres desvincular al usuario del rol?',
+
+    'The permission of the "{:parent}" is the parent of the "{:permission}"!' =>
+    '¡El permiso de "{:parent}" es el padre de "{:permission}"!',
+    'The role of the "{:parent}" is the parent of the "{:role}"!' =>
+    '¡El rol de "{:parent}" es el padre de "{:role}"!',
+
+    'The element will be irrevocably removed, which may affect some data. Do you really want to delete it?' =>
+    '¡El elemento se eliminará irrevocablemente, lo que puede afectar algunos datos. ¿Realmente quieres eliminarlo?',
+
+    'The role "{:name}" have been successfully deleted.' => '¡El rol "{:name}" ha sido eliminado con éxito!',
+    'The permission "{:name}" have been successfully deleted.' =>
+    '¡El permiso "{:name}" ha sido eliminado con éxito!',
+    'User "{:username}" successfully unassigned.' => '¡El usuario "{:username}" ha sido desasignado con éxito!',
+    'User "{:username}" is not attached to any role!' => '¡El usuario "{:username}" no está vinculado a ningún rol!',
+
+    'Error!' => 'Error!',
+    'The requested page does not exist.' => 'La página solicitada no existe.',
+    'You are not allowed access!' => 'No se te permite el acceso.',
+    'Fail!' => 'Fallo!',
+    'Done!' => 'Hecho!',
+    'Success!' => 'Éxito!',
+    'The user already has a role.' => 'El usuario ya tiene un rol.',
+    'Username:' => 'Nombre de usuario:',
+    'Role:' => 'Rol:',
+    'All Roles' => 'Todos los roles',
+    'User "{:Username}" not found' => 'Usuario "{:Username}" no encontrado',
+    'Access to the config site' => 'Acceso al sitio de configuración',
+    'Access to Comments management' => 'Acceso al administrador de comentarios',
+    'Access to Maintenance management' => 'Acceso al administrador de mantenimiento',
+    'View a site in maintenance mode' => 'Ver un sitio en modo de mantenimiento',
+    'Class {:userClassName} does not implement interface yii\web\IdentityInterface.' =>
+    'La clase {:userClassName} no implementa la interfaz yii\web\IdentityInterface.',
+    'Roles with permissions' => 'Roles con permisos',
+];
