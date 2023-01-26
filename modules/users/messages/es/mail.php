0d76706693d55421c6b442675be7acbff40a57a6
hectorsegarra <hector.segarra@outlook.com>
2023-01-26 10:10:38 +0100

    Traduccion a español

--- modules/users/messages/es/mail.php
+++ modules/users/messages/es/mail.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'HELLO {username}' => 'Hello {username}',
-    'FOLLOW_TO_RESET_PASSWORD' => 'Follow the link below to reset your password',
-    'FOLLOW_TO_CONFIRM_EMAIL' => 'Follow the link below to activate account',
-    'IGNORE_IF_DO_NOT_REGISTER' => 'If not registration site, to ignore this message.',
+    'HELLO {username}' => 'HOLA {username}',
+    'FOLLOW_TO_RESET_PASSWORD' => 'Sigue el enlace a continuación para restablecer tu contraseña',
+    'FOLLOW_TO_CONFIRM_EMAIL' => 'Sigue el enlace a continuación para activar tu cuenta',
+    'IGNORE_IF_DO_NOT_REGISTER' => 'Si no registraste en el sitio, ignora este mensaje.'
 ];
