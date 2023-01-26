696d6bfa900b80a689b006046a30fdd05c9162d4
hectorsegarra <hector.segarra@outlook.com>
2023-01-26 10:08:55 +0100

    Traduccion a Español del modulo users

---
+++ modules/users/messages/es/module.php
@@ -0,0 +1,152 @@
+<?php
+
+return [
+        'Login' => 'Iniciar sesión',
+    'Login to the site to start the session' => 'Inicie sesión en el sitio para iniciar la sesión',
+
+    'Remember Me' => 'Recuérdame',
+
+    'Users' => 'Usuarios',
+    'Guest' => 'Invitado',
+
+    'ID' => 'ID',
+    'Created' => 'Creado',
+    'Updated' => 'Actualizado',
+    'Last Visit' => 'Última visita',
+    'Username' => 'Nombre de usuario',
+    'Email' => 'Correo electrónico',
+    'Auth Key' => 'Clave de autenticación',
+    'Status' => 'Estado',
+    'First Name' => 'Nombre',
+    'Last Name' => 'Apellido',
+    'Role' => 'Rol',
+    'Current Password' => 'Contraseña actual',
+    'New Password' => 'Nueva contraseña',
+    'Repeat Password' => 'Repetir contraseña',
+    'Avatar' => 'Avatar',
+    'Hash Password' => 'Contraseña Hash',
+    'Password Token' => 'Token de contraseña',
+    'Email Confirm Token' => 'Token de confirmación de correo electrónico',
+    'User Role Name' => 'Nombre de rol de usuario',
+    'Email Gravatar' => 'Email Gravatar',
+
+    'Blocked' => 'Bloqueado',
+    'Active' => 'Activo',
+    'Wait' => 'Espera',
+    'Deleted' => 'Eliminado',
+
+    'Profile' => 'Perfil',
+    'Password' => 'Contraseña',
+
+    'Create' => 'Crear',
+    'Save' => 'Guardar',
+    'View' => 'Ver',
+    'Update' => 'Actualizar',
+    'Delete' => 'Eliminar',
+    'Sign In' => 'Iniciar sesión',
+    'Sign Out' => 'Cerrar sesión',
+    'Send' => 'Enviar',
+    'Generate' => 'Generar',
+    'Generate new key' => 'Generar nueva clave',
+
+    '- text -' => '- texto -',
+    '- all -' => '- todos -',
+    '- select -' => '- seleccionar -',
+
+    'It remains to activate the account.' =>
+        nl2br("Te hemos enviado una carta con un enlace para activar, una indicación de la dirección de correo electrónico de registro. Por favor revisa tu correo."), // phpcs:ignore
+    'Thank you for registering!' =>
+        nl2br("Gracias por registrarte, tu cuenta ha sido activada con éxito. Ahora puedes iniciar sesión utilizando tus detalles de inicio de sesión."), // phpcs:ignore
+    
+    'Error sending message!' => 'Error al enviar el mensaje!',
+    'Check your email for further instructions.' => 'Revisa tu correo electrónico para obtener más instrucciones.',
+    'Sorry, we are unable to reset password.' => 'Lo siento, no podemos restablecer la contraseña.',
+    
+    'Incorrect current password.' => 'Contraseña actual incorrecta.',
+    'Enter your current password.' => 'Ingresa tu contraseña actual.',
+    'Not all fields are filled in correctly.' => 'No todos los campos están llenos correctamente.',
+    
+    'Are you sure you want to delete the record?' => '¿Estás seguro de que quieres eliminar el registro?',
+    'Are you sure you want to delete the entry?' => '¿Estás seguro de que quieres eliminar la entrada?',
+    
+    'Password reset token cannot be blank.' => 'El token de restablecimiento de contraseña no puede estar en blanco.',
+    'Wrong password reset token.' => 'Token de restablecimiento de contraseña incorrecto.',
+    'Email confirm token cannot be blank.' => 'El token de confirmación de correo electrónico no puede estar en blanco.',
+    'Wrong Email confirm token.' => 'Token de confirmación de correo electrónico incorrecto.',
+    
+    'Password changed successfully.' => 'Contraseña cambiada con éxito.',
+    'Error! Password changed not successfully.' => 'Error! La contraseña no se cambió con éxito.',
+    'Profile successfully save.' => 'Perfil guardado con éxito.',
+    'The requested page does not exist.' => 'La página solicitada no existe.',
+    'There is no user with this e-mail.' => 'No hay ningún usuario con este correo electrónico.',
+    
+    'Password reset for' => 'Restablecimiento de contraseña para',
+
+    'Invalid email or password.' => 'Correo electrónico o contraseña inválidos.',
+    'You do not have rights, access is denied.' => 'No tienes derechos, el acceso es denegado.',
+
+    'This username already exists.' => 'Este nombre de usuario ya existe.',
+    'This email already exists.' => 'Este correo electrónico ya existe.',
+    'The message was successfully sent!' => '¡El mensaje se envió con éxito!',
+
+    'Sign Up' => 'Regístrate',
+    'Please fill in the following fields to sign up' => 'Por favor, llena los siguientes campos para registrarte',
+
+    'Error!' => '¡Error!',
+
+    'Click to change the status' => 'Haga clic para cambiar el estado',
+
+    'form of discharge' => 'forma de descarga',
+    'If you have forgotten your password, use {:Link}' => 'Si has olvidado tu contraseña, usa {:Link}',
+
+    'Password Reset Form' => 'Formulario de restablecimiento de contraseña',
+    'Enter your email address' => 'Ingresa tu dirección de correo electrónico',
+
+    'Reset Password' => 'Restablecer contraseña',
+    'Create a new password' => 'Crear una nueva contraseña',
+
+    'Member since' => 'Miembro desde',
+
+    'The user "{:name}" will be marked as deleted!' => 'El usuario "{:name}" será marcado como eliminado!',
+    'You won\'t be able to revert this!' => '¡No podrás revertir esto!',
+    'The user "{:name}" are marked as deleted.' => 'El usuario "{:name}" ha sido marcado como eliminado.',
+    'The user "{:name}" have been successfully deleted.' => 'El usuario "{:name}" ha sido eliminado con éxito.',
+
+    'This username is already taken.' => 'Este nombre de usuario ya está en uso.',
+    'This email is already taken.' => 'Este correo electrónico ya está en uso.',
+
+    'To change the avatar, please use the {:link} service.' => 'Para cambiar el avatar, por favor utilice el servicio {:link}.',
+
+    'Password:' => 'Contraseña:',
+    'More than 6 symbols' => 'Más de 6 símbolos',
+    'Status:' => 'Estado:',
+    'New password:' => 'Nueva contraseña:',
+    'Remove expired user {:Username}' => 'Eliminar usuarios expirados {:Username}',
+    'Cannot remove expired user {:Username}' => 'No se puede eliminar el usuario expirado {:Username}',
+    'Done!' => '¡Hecho!',
+    'Success!' => '¡Éxito!',
+    'User "{:Username}" not found' => 'Usuario "{:Username}" no encontrado.',
+    'User not found.' => 'Usuario no encontrado.',
+
+    'Confirm deleting profile' => 'Confirmar eliminación de perfil',
+    'Your profile has been successfully deleted!' => '¡Tu perfil ha sido eliminado con éxito!',
+
+    'Form successfully saved.' => 'Formulario guardado exitosamente.',
+    'Error! Failed to save the form.' => '¡Error! No se pudo guardar el formulario.',
+
+    'Send a link to activate your account.' => 'Enviar un enlace para activar tu cuenta.',
+    'Account activation!' => '¡Activación de cuenta!',
+
+    'Image' => 'Imagen',
+    'Crop Width' => 'Ancho del recorte',
+    'Crop Height' => 'Alto del recorte',
+    'Crop X' => 'Cortar X',
+    'Crop Y' => 'Cortar Y',
+    'Submit' => 'Enviar',
+    'File uploaded successfully.' => 'Archivo subido con éxito.',
+    'Failed to upload file.' => 'Error al subir el archivo.',
+    'User avatar successfully save.' => 'Avatar de usuario guardado con éxito.',
+    'Are you sure you want to delete the avatar?' => '¿Estás seguro de que deseas eliminar el avatar?',
+    'The following download extensions are allowed: {:ext}' => 'Las siguientes extensiones de descarga están permitidas: {:ext}',
+    'Successfully deleted.' => 'Eliminado con éxito.',
+];
