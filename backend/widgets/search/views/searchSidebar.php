6320c5a6b7742ab3fe879abcd010132f8f2dfc52
hectorsegarra <hector.segarra@outlook.com>
2023-02-23 14:29:06 +0100

    se agrega el icono de informacion para buscar

--- backend/widgets/search/views/searchSidebar.php
+++ backend/widgets/search/views/searchSidebar.php
@@ -1,4 +1,4 @@
-<form action="#" method="get" class="sidebar-form">
+<form action="" method="get" class="sidebar-form">
     <div class="input-group">
         <label for="q-input"></label>
         <input id="q-input" type="text" name="q" class="form-control"
@@ -9,5 +9,13 @@
                 <i class="fa fa-search"></i>
             </button>
         </span>
+        <span class="input-group-btn">
+            <a href="#">
+                <button type="button" title="Información sobre lo que se puede buscar aqui." name="info-search" id="info-search-btn" class="btn btn-flat">
+                    <i class="fa fa-info"></i>
+                </button>
+            </a>
+        </span>
     </div>
 </form>
+
